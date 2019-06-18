fdddbcca5e8a04eb584a8c95783a91393ea11e58
Kailash43 <24frontteam@gmail.com>
2019-06-18 21:33:07 +0530

    initial comit

---
+++ styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      },
+      welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+      },
+      instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+      },
+      labelStyle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+      },
+      verticalStyle: {
+          borderWidth: 1,
+          borderColor: '#e8e8e8',
+          width: 250,
+          marginBottom: 10,
+      }
+});
